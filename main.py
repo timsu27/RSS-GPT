34e38b543d29de7ff6ba4f906d81176fcf082bed
Jun Lin Su <omg80827@gmail.com>
2025-02-06 15:07:20 +0800

    Update main.py
    zh >> zh-tw
    修改promt 簡中 to 繁中

--- main.py
+++ main.py
@@ -155,10 +155,10 @@ def truncate_entries(entries, max_entries):
     return entries
 
 def gpt_summary(query,model,language):
-    if language == "zh":
+    if language == "zh-tw":
         messages = [
             {"role": "user", "content": query},
-            {"role": "assistant", "content": f"请用中文总结这篇文章，先提取出{keyword_length}个关键词，在同一行内输出，然后换行，用中文在{summary_length}字内写一个包含所有要点的总结，按顺序分要点输出，并按照以下格式输出'<br><br>总结:'，<br>是HTML的换行符，输出时必须保留2个，并且必须在'总结:'二字之前"}
+            {"role": "assistant", "content": f"請用繁體中文總結這篇文章，先提取出{keyword_length}個關鍵詞，在同一行內輸出，然後換行，用中文在{summary_length}字內寫一個包含所有要點的總結，按順序分要點輸出，並按照以下格式輸出'<br><br>總結:'，<br>是HTML的換行符，輸出時必須保留2個，並且必須在'總結:'二字之前"}
         ]
     else:
         messages = [
